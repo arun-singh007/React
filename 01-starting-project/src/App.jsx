00721d1839a07c1128cf3045b98150a590d5b839
arun-singh007 <krarun10199@gmail.com>
2025-11-08 20:00:24 +0530

    props concepts

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,17 +1,41 @@
 import reactImg from './assets/react-core-concepts.png'
+import {CORE_CONCEPTS} from './data.js'
 const data = ['React', 'JavaScript', 'HTML', 'CSS'];
 
 function getRandomItem() {
   const randomIndex = Math.floor(Math.random() * data.length);
   return data[randomIndex];
 }
+function CoreConcept({title, image, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
 function App() {
   return (
     <div>
      <Header />
       <main>
-        <h2>Time to get started!</h2>
-      </main>
+      <section id='core-concepts'>
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept
+            image={CORE_CONCEPTS[0].image}
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+          />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+
+
+        </ul>
+      </section>
+    </main>
     </div>
   );
 }
